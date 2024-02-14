da2e663745185f9f92120cc88ff71385a7160132
Dobrocsi Kornél (SZF_2022_2024) <dobrocsi.kornel@diak.szamalk-szalezi.hu>
2024-02-14 13:53:42 +0100

    first commit

---
+++ src/dinamikus/vezerlő/DinamikusVezerlő.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dinamikus.vezerlő;
+
+/**
+ *
+ * @author DobrocsiKornél(SZF_2
+ */
+public class DinamikusVezerlő {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
